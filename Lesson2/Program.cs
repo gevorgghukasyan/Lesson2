bdab384b69f97b29e057251783fb4c177821f369
igevorg <igevorg@outlook.com>
2024-09-10 19:43:09 +0400

    first commit

---
+++ Lesson2/Program.cs
@@ -0,0 +1,143 @@
+﻿//Console.WriteLine("SmartCode");
+
+//Console.WriteLine(10 + 6);
+//Console.WriteLine(10 - 6);
+//Console.WriteLine(10 * 6);
+//Console.WriteLine(10 / 2);
+//----------------------------------------------------
+//Console.WriteLine(10 % 8);
+//----------------------------------------------------
+//Console.WriteLine(5);
+//----------------------------------------------------
+
+//int age = 17;
+//int Age = 67;
+
+//Console.WriteLine(age);
+//Console.WriteLine(Age);
+//----------------------------------------------------
+
+
+//string text = "C#";
+
+//Console.WriteLine(text);
+
+//text = "TTYTYTTTTTTT";
+
+//Console.WriteLine(text);
+//----------------------------------------------------
+
+//int number1 = 14;
+//int number2 = 15;
+
+//Console.WriteLine(number1 > number2);
+//Console.WriteLine(number1 < number2);
+//Console.WriteLine(number1 == number2);
+//Console.WriteLine(number1 >= number2);
+//Console.WriteLine(number1 <= number2);
+//Console.WriteLine(number1 != number2);
+//----------------------------------------------------
+
+//int x = 10;
+//int y = 3;
+//
+//int z = x / y;
+//
+//Console.WriteLine(z);
+//----------------------------------------------------
+
+//Console.WriteLine();
+//bool b = true;
+
+//Console.WriteLine(b);
+//----------------------------------------------------
+
+//double d = 5.3d;
+//float f = 1.2f;
+
+//Console.WriteLine(d);
+//Console.WriteLine(f);
+//----------------------------------------------------
+
+//char c = '@';
+//int x = 10;
+//x = 15;
+//Console.WriteLine(x);
+//----------------------------------------------------
+
+//short s = -32768;
+//Console.WriteLine(s);
+
+//ushort us = 9;
+//long l = 1555555555555555;
+//ulong ul = 15555555555555555555;
+
+//Console.WriteLine(l);
+//Console.WriteLine(ul);
+
+//Console.WriteLine("Greq dzer anuny");
+//string name = Console.ReadLine();
+//Console.WriteLine("Barev: " + name);
+//Console.Write("Mutqagreq arajin tivy: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Mutqagreq erkrord tivy: ");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Gumary = " + (num + num1));
+
+
+//Console.Write("Mutqagreq arajin tivy: ");
+//int x = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Mutqagreq erkrord tivy: ");
+//int y = Convert.ToInt32(Console.ReadLine());
+
+//Console.Write("x + y = ");
+//Console.WriteLine(x + y);
+//Console.Write("x - y = ");
+//Console.WriteLine(x - y);
+//Console.Write("x / y = ");
+//Console.WriteLine(x / y);
+//Console.Write("x * y = ");
+//Console.WriteLine(x * y);
+
+//Console.WriteLine(-1 + 4 * 6);
+//byte s1 = 5;
+//byte s2 = 4;
+//byte s3 = 3;
+
+//byte sssss = Convert.ToByte(Console.ReadLine());
+//Console.WriteLine(sssss);
+//short s = 7;
+//short ss = 9;
+//short sss = Math.Max(s, ss);
+
+//double a = Math.Sqrt(64);
+
+//int sd = Math.Abs(9);
+
+//var ssa = Math.Round(-7.6);
+
+//double aa = Math.Pow(2, 4);
+//Console.WriteLine(aa);
+
+
+//int x = Convert.ToInt32(Console.ReadLine());
+//int y = Convert.ToInt32(Console.ReadLine());
+
+//char c = Convert.ToChar(Console.ReadLine());
+
+//if (c == '+')
+//{
+//	Console.WriteLine(x + y);
+//}
+//else if (c == '-')
+//{
+//	Console.WriteLine(x - y);
+//}
+//else if (c == '*')
+//{
+//	Console.WriteLine(x * y);
+//}
+//else if (c == '/')
+//{
+//	Console.WriteLine(x / y);
+//}
